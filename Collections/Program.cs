1575f2dea04bc56a2b22f86db3563b9694e78cf9
KauanIzidoro <cnttbussineskauan@gmail.com>
2024-09-26 23:35:05 -0300

    📚 code: finish homework

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -12,12 +12,11 @@ class Program
             new Product("Iphone 16", rnd.Next(499, 5000)*rnd.NextDouble()),
             new Product("MacBook M1", rnd.Next(99, 5000)*rnd.NextDouble()),
             new Product("Samsung Galaxy", rnd.Next(199, 5000)*rnd.NextDouble()),
-            new Product("10kg de frango", rnd.Next(299, 5000)*rnd.NextDouble()),
             new Product("Batata temperada", rnd.Next(399, 5000)*rnd.NextDouble())
         };
 
         // Primeira solução:
-        // OrderByDescending() é um método LING (Language-Integrated Query), 
+        // OrderByDescending() é um método LINQ (Language-Integrated Query), 
         // que são recursos da Classe Colletions e ajudam na tarefa de consultas, ordenações e outra operação semelhante.
         // Como parâmetro do método, note que existe uma expressão lambda (p => p.Price), que significa: 
         // para cada p de products, use p.Price para comparação.
@@ -42,20 +41,21 @@ class Program
         Console.WriteLine(products[^1]);
         Console.WriteLine(products[^2]);
 
+
         // Terceira Solução:
-        double[] res = new double[2] {0,0};
-        for (int i=0; i<products.Length; i++)
+        double[] res = new double[2] { 0, 0 };
+        for (int i = 0; i < products.Length; i++)
         {
-            for (int j=i+1;j<products.Length;j++)
+            for (int j = i + 1; j < products.Length; j++)
             {
-                double aux_maior = (products[i].Price + products[j].Price + Math.Abs(products[i].Price - products[j].Price))/2;
+                double aux_maior = (products[i].Price + products[j].Price + Math.Abs(products[i].Price - products[j].Price)) / 2;
 
                 if (aux_maior > res[0])
                 {
                     res[1] = res[0];
                     res[0] = aux_maior;
                 }
-                else if(aux_maior > res[1])
+                else if (aux_maior > res[1])
                 {
                     res[1] = aux_maior;
                 }
@@ -68,7 +68,62 @@ class Program
         //     Console.WriteLine(res[l]);
 
         // }
-        Console.WriteLine(res[0] + "," + res[1]);
+        // Console.WriteLine(Math.Round(res[0],2) + "," + Math.Round(res[1],2));
+        for (int k = 0; k < products.Length; k++)
+        {
+            if (products[k].Price == res[0] || products[k].Price == res[1])
+            {
+                products[k].Price = 0.0;
+            }
+        }
+        double[] aux_products = new double[products.Length];
+        for (int i = 0; i < products.Length; i++)
+        {
+            aux_products[i] = products[i].Price;
+        }
+        // for (int i = 0; i < aux_products.Length; i++)
+        // {
+        //     Console.WriteLine(aux_products[i]);
+        // }
+
+        double[] ans = new double[2] { 0, 0 };
+        for (int i = 0; i < products.Length; i++)
+        {
+            for (int j = i + 1; j < products.Length; j++)
+            {
+                double aux_maior = (aux_products[i] + aux_products[j] + Math.Abs(aux_products[i] - aux_products[j])) / 2;
+
+                if (aux_maior > res[0])
+                {
+                    ans[1] = ans[0];
+                    ans[0] = aux_maior;
+                }
+                else if (aux_maior > ans[1])
+                {
+                    ans[1] = aux_maior;
+                }
+
+            }
+        }
+        // Console.WriteLine(Math.Round(ans[1],2) + " , " + Math.Round(ans[0],2));
+        // Console.WriteLine(Math.Round(res[0],2) + " , " +  Math.Round(res[1],2));
+
+        // Produto mais caro
+        for(int i=0;i<products.Length;i++)
+        {
+            if(products[i].Price == 0)
+            {
+                Console.WriteLine("Product: " + products[i].Name + ", Price: " + Math.Round(res[0],2));
+            }
+        }
+        // Segundo produto mais caro
+        for(int i=0;i<products.Length;i++)
+        {
+            if(products[i].Price == ans[1])
+            {
+                Console.WriteLine("Product: " + products[i].Name + ", Price: " + Math.Round(ans[1],2));
+            }
+        }
 
 
 
