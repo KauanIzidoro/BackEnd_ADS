fb50f659c89d78e5efa433e03a18eedec11f0742
KauanIzidoro <cnttbussineskauan@gmail.com>
2024-09-29 13:22:12 -0300

    📚 code: start program file

---
+++ CollectionsClass/class3_exe1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // Algumas configurações anteriores para implementar a solução do Exercício 1
+        Random rnd = new Random();
+        string[] products_name = {"Iphone 16", "Macbook M1", "Apple Watch", "Ventilador de teto", "Container Docker"};
+        
+    }
+}
